bfc24f34d612b5dfca0407489f03b8f711380bd4
Ivan Belenkov <belenkov.i.j@gmail.com>
2024-02-10 13:39:25 +0100

    adds jest support

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const { resolve } = require('path');
+
+module.exports = {
+  roots: [resolve(__dirname, 'src')],
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
+  }
+};
